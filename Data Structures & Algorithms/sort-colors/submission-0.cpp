ef9328383f7c3a90bfbf4814c8542eb81badb2d1
akshaya78010 <147473029+akshaya78010@users.noreply.github.com>
2026-06-11 15:14:03 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void merge(vector<int>&arr,int low,int mid,int high){
+        int i = low;
+        int j = mid + 1;
+
+        vector<int>temp;
+        while(i <= mid && j <= high){
+            if(arr[i] <= arr[j]){
+                temp.push_back(arr[i++]);
+            }
+            else{
+                temp.push_back(arr[j++]);
+            }
+        }
+
+        while(i <= mid){
+            temp.push_back(arr[i++]);
+        }
+
+        while(j <= high){
+            temp.push_back(arr[j++]);
+        }
+
+        for(int k = 0 ; k < temp.size() ; k++){
+            arr[low + k] = temp[k];
+        }
+
+        return;
+    }
+
+    void merge_sort(vector<int>&nums,int low,int high){
+        if(low >= high) return;
+        
+        int mid = low + (high - low)/2;
+        merge_sort(nums,low,mid);
+        merge_sort(nums,mid + 1,high);
+
+        merge(nums,low,mid,high);
+    }
+    void sortColors(vector<int>& nums) {
+        
+        merge_sort(nums,0,nums.size() - 1);
+    }
+};
