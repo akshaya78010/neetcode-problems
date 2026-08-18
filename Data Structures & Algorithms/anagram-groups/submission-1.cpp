4aa98de18a0e19dfca187ea40a293fb7dff1b249
akshaya78010 <147473029+akshaya78010@users.noreply.github.com>
2026-08-18 22:50:20 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string,vector<string>>mpp;
+
+        for(int i =0; i < strs.size() ; i++){
+            
+            string word = strs[i];
+            sort(word.begin(),word.end());
+            mpp[word].push_back(strs[i]);
+        }
+
+        vector<vector<string>>res;
+
+        for(auto it = mpp.begin() ; it != mpp.end() ; it++){
+          
+            res.push_back(it->second);
+        }
+
+        return res;
+    }
+};
